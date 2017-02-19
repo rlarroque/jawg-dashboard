df2e0747ccb1c01ad46993f1633c98cf2e0feeb7
rlarroque <rlarroque@gmail.com>
2017-02-19 15:10:32 +0100

    [feat] sonar widget

--- jobs/sonar.rb
+++ jobs/sonar.rb
@@ -3,17 +3,29 @@ require 'json'
 
 server = "http://sonar.r7.jawg.io"
 key = "io.jawg:jawg-stats-api"
-metrics = %w{overall_coverage}.join(',')
+metrics = %w{files,lines,overall_coverage,open_issues}.join(',')
 id = "sonar"
 
 #http://sonar.r7.jawg.io/api/resources?resource=io.jawg:jawg-stats-api&metrics=Coverage&format=json
 #http://sonar.r7.jawg.io/api/measures/component?componentKey=io.jawg:jawg-stats-api&metricKeys=overall_coverage&format=json
 
-SCHEDULER.every '10s', :first_in => 0 do |job|
+sonar_metrics = ['Number of files', 'Number of lines', 'Code coverage', 'Issues']
+
+SCHEDULER.every '1m', :first_in => 0 do |job|
   uri = URI("#{server}/api/measures/component?componentKey=#{key}&metricKeys=#{metrics}&format=json")
 	res = Net::HTTP.get(uri)
-  j = JSON[res]['component']['measures']
+  measures = JSON[res]['component']['measures']
+  sonar_metrics_measues = []
 
-puts j[0]['value']
-	send_event('sonar', current: j[0]['value'])
+  measures.each do |child|
+    metric = child['metric']
+    value = child['value']
+    if (metric == 'overall_coverage')
+      sonar_metrics_measues << { label: child['metric'], value: child['value'] + '%'}
+    else
+      sonar_metrics_measues << { label: child['metric'], value: child['value'] }
+    end
+  end
+
+	send_event('sonar', {items: sonar_metrics_measues})
 end
